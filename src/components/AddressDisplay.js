2def2e3752e66c60dcb5f5d3a64e9e76684f8222
dandotcraig <145010207+dandotcraig@users.noreply.github.com>
2024-05-28 19:49:39 +1000

    pre-moving-data-to-context

--- src/components/AddressDisplay.js
+++ src/components/AddressDisplay.js
@@ -1,6 +1,7 @@
 import React from "react";
 
 export default function AddressDisplay({clientSent}) {
+    console.log(clientSent.value)
     return (
         <div className="mt-1 text-sm leading-6 text-gray-600">
             {clientSent}
